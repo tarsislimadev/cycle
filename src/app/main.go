d0223640e2dac02dea560208ca92b413bec6e928
Tarsis de Lima <br.tmvdl@gmail.com>
2023-01-17 08:10:54 -0300

    Fixing app files

--- src/app/main.go
+++ src/app/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"log"
+	"net"
+	"bufio"
+)
+
+func panicError(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(bufio.NewReader(conn))
+
+	for scanner.Scan() {
+		log.Println(scanner.Text())
+	}
+}
 
 func main() {
-	fmt.Println("Hello world.")
+	port := os.Getenv("PORT")
+
+	ln, err := net.Listen("tcp", ":" + port)
+	panicError(err)
+
+	for {
+		conn, err := ln.Accept()
+		panicError(err)
+
+		go handleConnection(conn)
+	}
 }
